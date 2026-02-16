0711679660be068959202c803fc449156290b3b3
Abhishek Yadav <yabhishekk480@gmail.com>
2026-02-16 10:32:19 +0530

    Cleared project

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `Hello &{person}, welcome`;
-}
-const username = "abhishek";
-console.log(greet(username));
-//# sourceMappingURL=index.js.map
